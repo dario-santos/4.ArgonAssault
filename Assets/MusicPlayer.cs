50b916b3bf590e041b57a6750f06d1ac858ba6b1
Dário Santos <dariovfsantos@gmail.com>
2018-04-20 00:24:24 +0100

    Added Sound and splash screen

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	[SerializeField]private float waitTime = 5f;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadFirstScene", waitTime);
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	private void LoadFirstScene() 
+	{
+		SceneManager.LoadScene(1); //Load level 1;
+	}
+	
+}
